0c52161039df3c711122e5c59d008793d52a283e
carlosrodriguez <carlos.rodriguez@adetec.com>
2024-02-15 16:53:39 +0100

    first commit

---
+++ test/test_dummy/test_dummy.c
@@ -0,0 +1,27 @@
+#include <unity.h>
+
+void test_dummy(void)
+{
+  TEST_ASSERT_EQUAL(1, 1);
+}
+
+void test_dummy2(void)
+{
+  TEST_ASSERT_TRUE(1);
+}
+
+void test_dummy3(void)
+{
+  TEST_ASSERT_FALSE(0);
+}
+
+void app_main()
+{
+  UNITY_BEGIN();
+
+  RUN_TEST(test_dummy);
+  RUN_TEST(test_dummy2);
+  RUN_TEST(test_dummy3);
+
+  UNITY_END();
+}
